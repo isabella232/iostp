4da11052201dc63a439bb155a3d94003f12f69ac
robertlight <light@alum.mit.edu>
2013-08-20 13:12:28 -0400

    1st commit

---
+++ install/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+//Database Information
+$db_host = "localhost"; //Host address (most likely localhost)
+$db_name = "iostp";     //Name of Database
+$db_user = "root";      //Name of database user
+$db_pass = "s3cr3t";    //Password for database user
+
+$stackDir= "/home/light/dev/xively/lampstack-5.4.17-2";
+
+$webhost_port = "8083";
+
+$webhost_name = "localhost";
+
+?>
