9e2b96b8d3a504ba42208b46a3436c01af820f12
robertlight <light@alum.mit.edu>
2013-10-10 16:56:22 -0400

    installation

--- src/iostp/sessionTimedout.php
+++ src/iostp/sessionTimedout.php
@@ -1,23 +1,16 @@
 <?php
-// Initialize the session.
-// If you are using session_name("something"), don't forget it now!
-session_start();
+require_once("userAdmin/models/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
 
-// Unset all of the session variables.
-$_SESSION = array();
-
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+//Log the user out
+if(isUserLoggedIn())
+{
+    trigger_error("Logging user '".$loggedInUser->username."' out.",E_USER_NOTICE);
+	$loggedInUser->userLogOut();
+} else {
+    trigger_error("User '".$loggedInUser->username."' not logged in", E_USER_ERROR);
 }
 
-// Finally, destroy the session.
-session_destroy();
 ?>
 <html>
 <body>
